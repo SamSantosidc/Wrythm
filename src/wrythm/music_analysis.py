dbc967f7bcd78d5f3571c27ec6c5f344090a22a1
Samuel Santos <samuel.santos@dcx.ufpb.br>
2025-08-20 15:57:27 -0300

    feat: Separação do sinal em componentes harmônicos e percurssivos.

--- src/wrythm/music_analysis.py
+++ src/wrythm/music_analysis.py
@@ -22,9 +22,15 @@ def extract_mfcc(y, sr, n_mfcc=13):
     """Extrai coeficientes cepstrais de Mel (MFCCs)."""
     return librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
 
+
 def extract_spectral_features(y, sr):
     """Extrai centroid, bandwidth e contraste espectral."""
     spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
     spectral_bandwidth = librosa.feature.spectral_bandwidth(y=y, sr=sr)
     spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr)
     return spectral_centroid, spectral_bandwidth, spectral_contrast
+
+
+def harmonic_percussive(y):
+    """Separa sinal em componentes harmônicos e percussivos."""
+    return librosa.effects.hpss(y)
