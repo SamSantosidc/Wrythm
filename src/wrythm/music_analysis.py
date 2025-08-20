b8c595bde58926af1e9a4212fef6d66f49f71d1e
Samuel Santos <samuel.santos@dcx.ufpb.br>
2025-08-20 15:52:11 -0300

    feat: Extração de MFCCs.

--- src/wrythm/music_analysis.py
+++ src/wrythm/music_analysis.py
@@ -17,3 +17,7 @@ def extract_chroma(y, sr):
     """Extrai cromagrama (energia de cada classe de nota ao longo do tempo)."""
     chroma = librosa.feature.chroma_cqt(y=y, sr=sr)
     return chroma
+
+def extract_mfcc(y, sr, n_mfcc=13):
+    """Extrai coeficientes cepstrais de Mel (MFCCs)."""
+    return librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
