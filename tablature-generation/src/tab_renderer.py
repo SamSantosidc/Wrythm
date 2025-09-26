3f9daa42b55afbe425961d5f393eecdf2aee061d
Samuel Santos <samuel.santos@dcx.ufpb.br>
2025-09-26 00:27:41 -0300

    refactor: Refact Tab renderization function.

--- tablature-generation/src/tab_renderer.py
+++ tablature-generation/src/tab_renderer.py
@@ -1,13 +1,15 @@
 def render_tab(note_sequence):
     """
-    note_sequence: lista de (time, string, fret)
+    Constrói tablatura a partir de uma sequência de (tempo, corda, casa).
     """
-    tab_lines = {i: [] for i in range(1, 7)}
+    tab_lines = {i: [] for i in range(1, 7)}  # 6 cordas
+
     for _, string, fret in note_sequence:
         for s in tab_lines:
             tab_lines[s].append(str(fret) if s == string else "-")
 
+    # monta saída como texto
     tab_output = ""
-    for s in sorted(tab_lines.keys(), reverse=True):
+    for s in sorted(tab_lines.keys(), reverse=True):  # da 1ª à 6ª corda
         tab_output += f"{s}|{''.join(tab_lines[s])}\n"
     return tab_output
